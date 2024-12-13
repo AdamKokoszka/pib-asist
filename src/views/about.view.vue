1690a35ccc941246161f9601dfe8d761f3fb2fde
adamkokoszka <adam.kokoszka@antologic.com>
2024-12-13 12:02:45 +0100

    Response classes structure

--- src/views/about.view.vue
+++ src/views/about.view.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
     <h1>This is an about page</h1>
+
+    <div class="block w-full text-sm
+                sm:w-20
+                md:w-40 md:text-xl
+                lg:w-72">
+
+      <p>Response block</p>
+    </div>
+
+    <div class="block w-full text-sm sm:w-20 md:w-40 md:text-xl lg:w-72">
+      <p>Response block w</p>
+    </div>
   </div>
 </template>
