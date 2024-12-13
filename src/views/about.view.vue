b026aad57cc63fd3cfc2415bab99575d5a9b7b3c
adamkokoszka <adam.kokoszka@antologic.com>
2024-12-13 12:27:05 +0100

    Add Lodash

--- src/views/about.view.vue
+++ src/views/about.view.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import shuffle from 'lodash-es/shuffle'
+import { ref } from 'vue'
+
+const array = [1, 2, 3, 4]
+const shuffledArray = ref(shuffle(array))
+</script>
+
 <template>
   <div>
     <h1>This is an about page</h1>
@@ -13,5 +21,7 @@
     <div class="block w-full text-sm sm:w-20 md:w-40 md:text-xl lg:w-72">
       <p>Response block w</p>
     </div>
+
+    <p>{{ shuffledArray }}</p>
   </div>
 </template>
